fc5b0421adc33330688621aee17cb9c0674fd720
AtenB3f <atenb3f@gmail.com>
2022-01-27 16:14:07 +0900

    Feat: Firebase task, habit데이터 테스트 경로로 데이터 올리는 기능 추가

--- BikPik/Utilities/Firebase.swift
+++ BikPik/Utilities/Firebase.swift
@@ -20,7 +20,37 @@ class Firebase {
     func uploadTask(task: Task) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
-        //self.ref.child("test/users/\(uid)/tasks/\(task.date)").setValue(task)
+        let taskRef = self.ref.child("test/users/\(uid)/tasks/\(task.date)/\(task.name!)_\(task.id)")
+        
+        setRefTask(task: task, ref: taskRef)
+    }
+    
+    private func setRefTask(task: Task, ref: DatabaseReference) {
+        ref.child("time").setValue(task.time)
+        ref.child("alram").setValue(task.alram)
+        ref.child("inToday").setValue(task.inToday)
+        ref.child("isDone").setValue(task.isDone)
+        if task.tag != nil {
+            ref.child("tag").setValue(task.tag)
+        }
+        if task.color != nil {
+            ref.child("color").setValue(task.color)
+        }
+    }
+    
+    func uploadHabit(habit: Habits) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let habitRef = self.ref.child("test/users/\(uid)/haibts/\(habit.task.name!)")
+        
+        habitRef.child("start").setValue(habit.start)
+        habitRef.child("end").setValue(habit.end)
+        habitRef.child("days").setValue(habit.days)
+        if habit.isDone != nil {
+            habitRef.child("isDone").setValue(habit.isDone)
+        }
+        
+        setRefTask(task: habit.task, ref: habitRef)
     }
     
 }
